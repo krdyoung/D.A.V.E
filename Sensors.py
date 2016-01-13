acae171ff8ef172a4125835bc6e52f9953ea75d3
Kieran Young <Kieran@Kieran.lan>
2016-01-13 20:13:49 +0000

    gitignore

--- Sensors.py
+++ Sensors.py
@@ -1,9 +1,37 @@
 import time
 import RPi.GPIO as GPIO
 
+#Front Sensor
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(24, GPIO.IN)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.IN)
+
+#Back Sensor
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.IN)
+
+#Bottom Sensor
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(12, GPIO.IN)
+
+def read_front():
+    GPIO.output(17, True)
+    time.sleep(0.005)
+    GPIO.output(17, False)
+
+    while GPIO.input(27) == 0:
+        front_signal_off = time.time()
+
+    while GPIO.input(27) == 1:
+        front_signal_on = time.time()
+
+    front_time_passed = front_signal_on - front_signal_off
+    front_distance = front_time_passed * 17000
+    return front_distance
+
+
 
 def read_distance():
     GPIO.output(23, True)
